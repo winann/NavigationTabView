596c18bfd164b29e18852f0d2860ae7e61682c23
Ann <sya@mogoroom.com>
2018-08-31 15:47:32 +0800

    add tab to view

--- NavigationTabView/Classes/WillAddNavigationTab.swift
+++ NavigationTabView/Classes/WillAddNavigationTab.swift
@@ -11,17 +11,18 @@ public protocol WillAddNavigationTab {
     
 }
 extension WillAddNavigationTab where Self: UIViewController {
-    public func addNavigationTab(for titles: [String], selectIndex: Int = 0) -> NavigationTabView? {
-        guard let nav = self.navigationController else { return nil }
-        nav.setNavigationBarHidden(false, animated: false)
-//        var frame = CGRect(x: (UIScreen.main.bounds.width - tabWidth) / 2 , y: 20, width: tabWidth, height: 44)
-//        if UIScreen.main.bounds.height == 812 {
-//            frame.origin.y = 44
-//        }
-//        frame.origin = CGPoint(x: 0, y: 0)
-        let frame = CGRect(x: 0, y: 0, width: tabWidth, height: 44)
+    public func addNavigationTab(for titles: [String], selectIndex: Int = 0, to view: UIView? = nil, viewWidth: CGFloat = UIScreen.main.bounds.width) -> NavigationTabView? {
+        var frame = CGRect(x: 0, y: 0, width: tabWidth, height: 44)
         let navigationTabView = NavigationTabView(frame: frame, titles: titles, selectIndex: selectIndex)
-        navigationItem.titleView = navigationTabView
+        if let superView = view {
+            print("\(superView.frame)-----------\(superView.bounds)")
+            frame.origin.x = (viewWidth - tabWidth) / 2
+            frame.origin.y = superView.bounds.maxY - 44
+            navigationTabView.frame = frame
+            superView.addSubview(navigationTabView)
+        } else {
+            navigationItem.titleView = navigationTabView
+        }
         return navigationTabView
     }
 }
